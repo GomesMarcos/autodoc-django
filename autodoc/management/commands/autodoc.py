599c38368dd83dc47d6c72e037405e2b137ec8cf
GomesMarcos <marconfirm@gmail.com>
2024-10-31 22:34:11 -0300

    feat: WIP getting some file information

--- autodoc/management/commands/autodoc.py
+++ autodoc/management/commands/autodoc.py
@@ -8,14 +8,17 @@ class Command(BaseCommand):
     help = "Generate MermaidJS (https://mermaid.live/edit) flowchart auto documentation to django apps' files"
 
     def handle(self, *args, **kwargs):
-        print(args, kwargs)
-        if args in settings.INSTALLED_APPS:
+        print(args)
+        if settings.INSTALLED_APPS in args:
             print('qwe')
         else:
             print(f'\033[91m\n\nInvalid arg: {args} \n\n\033[0m')
 
+        print(self.main())
+
     def generate_mermaid_diagram(self, filename):
-        """Gera um diagrama Mermaid para o arquivo Python especificado.
+        """Gera um diagrama Mermaid par
+        a o arquivo Python especificado.
 
         Args:
             filename (str): Caminho completo para o arquivo Python.
@@ -28,9 +31,26 @@ class Command(BaseCommand):
             tree = ast.parse(f.read())
 
         mermaid_code = 'flowchart\n'
-        for node in ast.walk(tree):
-            if isinstance(node, ast.FunctionDef):
-                mermaid_code += f'{node.name}(Function)\n'
+        for branch in ast.walk(tree):
+            if hasattr(branch, 'body') and isinstance(branch, ast.Module):
+                imports = tuple(
+                    node.module
+                    for node in branch.body
+                    if isinstance(node, ast.ImportFrom)
+                )
+                print('Imports', imports, '\n\n')
+            if isinstance(branch, ast.ClassDef):
+                print('Class', branch.name)
+
+            if isinstance(branch, ast.FunctionDef):
+                print(
+                    '\tFunction:',
+                    branch.name,
+                    tuple(arg.arg for arg in branch.args.args),
+                )
+                if branch.body:
+                    # TODO: find a way to readable return in here
+                    print('Return:', ast.dump(branch.body[0].value), '\n')
 
         return mermaid_code
 
@@ -51,7 +71,7 @@ class Command(BaseCommand):
 
         # TODO: get app name by arg and it's views.py, admin.py files.
         # TODO: get specified file's paths list
-        python_files = ['autodoc.py']
+        python_files = ['test.py']
 
         for file in python_files:
             mermaid_code = self.generate_mermaid_diagram(file)
